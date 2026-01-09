cdf9510b1a4bb26485f4bbdff43da7703eebc369
이창우 <cwadven@naver.com>
2026-01-09 09:58:51 +0900

    cc
    cc

--- common/common_paginations/cursor_pagination_helpers.py
+++ common/common_paginations/cursor_pagination_helpers.py
@@ -7,19 +7,26 @@ from typing import (
 from common.common_criteria.cursor_criteria import CursorCriteria
 from django.db.models import (
     QuerySet,
+    Q,
 )
 
 
-def get_objects_with_cursor_pagination(qs: QuerySet,
-                                       cursor_criteria: Type[CursorCriteria],
-                                       filtering_operator: dict,
-                                       size: int) -> tuple[List[Any], bool, str]:
+def get_objects_with_cursor_pagination(
+        qs: QuerySet,
+        decoded_next_cursor: dict,
+        size: int,
+        cursor_criteria: Type[CursorCriteria],
+) -> tuple[List[Any], bool, str]:
+    if decoded_next_cursor:
+        filter_q: Q = cursor_criteria.get_filter_q(decoded_next_cursor)
+        if filter_q:
+            qs = qs.filter(filter_q)
+    ordering_data = cursor_criteria.get_ordering_data()
+    if ordering_data:
+        qs = qs.order_by(*ordering_data)
+
     objects = list(
-        qs.filter(
-            **filtering_operator
-        ).order_by(
-            *cursor_criteria.get_ordering_data()
-        )[:size + 1]
+        qs[:size + 1]
     )
     has_more = bool(len(objects) > size)
     paginated_objects = objects[:size]
@@ -27,7 +34,7 @@ def get_objects_with_cursor_pagination(qs: QuerySet,
         paginated_objects,
         has_more,
         (
-            cursor_criteria.get_encoded_base64_cursor_data(paginated_objects[-1])
+            cursor_criteria().get_encoded_base64_cursor_data(paginated_objects[-1])
             if has_more else None
         ),
     )
